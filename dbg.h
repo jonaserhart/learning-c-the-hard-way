eecf105bd6ee9f6468a2f083500cb0fa9690008f
jonaserhart <jonas.a.erhart@gmail.com>
2020-12-01 21:56:37 +0100

    used argument of logfind

--- dbg.h
+++ dbg.h
@@ -15,19 +15,25 @@
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
 #define log_err(M, ...) fprintf(stderr,\
-				"[ERROR] (%s:%d:%s: errno: %s) " M "\n", __FILE__, __LINE__, __PRETTY_FUNCTION__,\
+				"[ERROR] (%s:%d:%s: errno: %s) " M "\n", \
+				__FILE__, __LINE__, __PRETTY_FUNCTION__, \
 				clean_errno(), ##__VA_ARGS__)
 
 #define log_warn(M, ...) fprintf(stderr,\
-				 "[WARN] (%s:%d:%s: errno: %s) " M "\n", __FILE__, __LINE__,__PRETTY_FUNCTION__, \
+				 "[WARN] (%s:%d:%s: errno: %s) " M "\n", \
+				 __FILE__, __LINE__,__PRETTY_FUNCTION__, \
 				 clean_errno(), ##__VA_ARGS__)
 
 #define log_info(M, ...) fprintf(stderr,\
-				 "[INFO] (%s:%d:%s) " M "\n", __FILE__, __LINE__, __PRETTY_FUNCTION__, ##__VA_ARGS__)
+				 "[INFO] (%s:%d:%s) " M "\n", \
+				 __FILE__, __LINE__, __PRETTY_FUNCTION__, ##__VA_ARGS__)
 
 #define check(A, M, ...) if (!(A)) {\
     log_err(M, ##__VA_ARGS__); errno=0; goto error;}
 
+#define check_continue(A, M, ...) if(!(A)) {\
+    log_warn(M, ##__VA_ARGS__); goto cont;}
+  
 #define sentinel(M, ...) {log_err(M, ##__VA_ARGS__);\
     errno=0; goto error;}
 
--- logfind.c
+++ logfind.c
@@ -7,8 +7,7 @@
 #include "dbg.h"
 
 const char * PATHS[] = {
-			"/var/log",
-			"/home/jonas/dev/learning-c-the-hard-way"
+			"/home/jonas/dev/logfilestest"
 };
 
 int and(int x, int y){
@@ -19,66 +18,95 @@ int or(int x, int y){
   return (x || y);
 }
 
+int check_directory(const char* path, const char* find){
+  struct dirent *dir_entry;
+  DIR *dir = NULL;
+  FILE *file = NULL;
+  char *line = NULL;
+  size_t length = 0;
+  ssize_t read = 0;
+  char *full_path = NULL;
+  int rc = 0;
+  
+  dir = opendir(path);
+
+  check(dir != NULL, "Cannot open directory %s", path);
+
+  while((dir_entry = readdir(dir)) != NULL){
+    char *full_path = calloc(
+			     sizeof(*path)/sizeof(char)
+			     + sizeof(dir_entry->d_name)/sizeof(char) + 2,
+			     sizeof(char));
+    check_mem(full_path);
+    
+    strcat(full_path, path);
+    strcat(full_path, "/");
+    strcat(full_path, dir_entry->d_name);
+
+    if (dir_entry->d_type != DT_DIR){
+
+      file = fopen(full_path, "r");
+      check_continue(file != NULL, "File '%s'  could not be opened", full_path);
+	
+      log_info("Searching in file '%s'", full_path);
+	
+      int line_number = 0;
+      while((read = getline(&line, &length, file)) != -1){
+        if(strstr(line, find)){
+          log_info("found in %s: line %d", full_path, line_number+1);
+        }
+        line_number++;
+      }
+      goto cont;
+    cont:
+      rc = fclose(file);
+      check(rc == 0, "Failed to close file stream");
+      free(full_path);
+    }
+    else{
+      if(strcmp(dir_entry->d_name, ".") != 0 && strcmp(dir_entry->d_name, "..") != 0)
+	check_directory(full_path, find);
+    }
+  }
+    closedir(dir);
+    return 0;
+ error:
+    if (full_path) free(full_path);
+    if(file) fclose(file);
+    if(dir) closedir(dir);
+    return -1;
+}
+
 typedef int (*operator_func) (int x, int y);
 
 int main (int argc, char* argv[]){
   
-  check(argc <= 3, "USAGE: logfind [-o(optional)] ...words");
+  check(argc >= 2, "USAGE: logfind [-o(optional)] [...words]");
 
   operator_func op = NULL;
+
+  char* strToFind;
   
   if(strcmp(argv[1], "-o") == 0){
     op = or;
+    strToFind = argv[2];
   }
   else{
     op = and;
+    strToFind = argv[1];
   }
 
   check(op != NULL, "Could not set operation");
 
-  struct dirent *dir_entry;
-  DIR *dir;
-
   int num_of_paths = sizeof(PATHS)/sizeof(char*);
-  int i = 0;
-  char **files;
+  
   for (int j = 0; j < num_of_paths; j++){
-  
-    dir = opendir(PATHS[j]);
-
-    check(dir != NULL, "Cannot open directory %s", PATHS[j]);
-
-    files = (char**)calloc(100, sizeof(char**));
-    i = 0;
-    while((dir_entry = readdir(dir)) != NULL){
-      if (dir_entry->d_type != DT_DIR){ 
-	files[i] = (char*)malloc(sizeof(char)*(strlen(dir_entry->d_name) + 1));
-	check_mem(files[i]);
-	strcpy(files[i], dir_entry->d_name);
-	i++;
-	files = (char**)reallocarray(files, i+1, sizeof(char**));
-      }
-    }
-
-    for (int j = 0; j < i; j++){
-      log_info("Searching in file: %s", files[j]);
-    }
-  }
-
-  closedir(dir);
-
-  for(int j = 0; j < i; j++){
-    free(files[j]);
-  }
-  
+    check_directory(PATHS[j], strToFind);
+  }   
   return 0;
 
  error:
-  if (dir) closedir(dir);
-  if (files){
-    for(int j = 0; j < i; j++){
-      free(files[j]);
-    }
-  }
   return -1;
 }
+
+
